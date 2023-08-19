f2d715bc647d93ab15732f066a8de512d103366c
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2023-08-19 23:47:32 +0600

    code changed

---
+++ js/common-function.js
@@ -0,0 +1,6 @@
+function getCardPrice(textId) {
+    const cardPrice = document.getElementById(textId);
+    const cardPriceString = cardPrice.innerText;
+    const price = parseFloat(cardPriceString);
+    return price;
+}
